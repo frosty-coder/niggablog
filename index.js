2301bdef09933f56d4427672e90a54ea87ae2032
frost <frostydread@outlook.com>
2024-12-05 20:34:49 +0000

    hi

---
+++ index.js
@@ -0,0 +1,48 @@
+/*//console.log("hello");
+//window.alert("his is an alart");
+document.getElementById(`myh1`).textContent = `Welcome to the C`;
+document.getElementById(`myp`).textContent = `we are sigma`; 
+
+let x;
+x = 100
+
+console.log(x) 
+
+
+let come = true;
+
+console.log(` will bro come true or false = ${come}`)  
+
+
+
+let title = "flixeon";
+let adimnuber = 1209;
+let option = true;
+
+document.getElementById(`p1`).textContent = title;
+document.getElementById(`p2`).textContent = adimnuber;
+document.getElementById(`p3`).textContent = option; 
+
+let students = 30;
+
+students = students + 1
+students += 2
+
+
+console.log(students); 
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result)
+
+
+console.log(username);
+let username = window.prompt("what your username?") 
+
+console.log(username); 
+let username;
+
+document.getElementById("mysubmit").onclick = function(){
+    username = document.getElementById(`mytext`).value;
+    document.getElementById(`myh1`).textContent = `hello ${username}`
+} */
